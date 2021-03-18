412c85f0d71afa2d2c569306dc1f16b24c248b53
jcansdale-test <60072076+jcansdale-test@users.noreply.github.com>
2021-03-18 11:10:51 +0000

    Include source in repo

---
+++ src/main/java/com/octokat/app/PublishApp.java
@@ -0,0 +1,11 @@
+package com.octokat.app;
+
+import com.octokat.app.PublishApp;
+
+public class InstallApp
+{
+    public static void main( String[] args )
+    {
+        PublishApp.main( new String[0] );
+    }
+}
